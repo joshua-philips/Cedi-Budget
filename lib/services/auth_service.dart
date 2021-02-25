f52c7ca6205350d8dcec2b1cf73aa276491f68e8
joshuaphilips <philipsjoshua96@gmail.com>
2021-02-25 17:21:02 +0000

    flutterfire | my provider | sign in methods

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Stream<String> get onAuthStateChanged => _firebaseAuth.authStateChanges().map(
+        (user) => user?.uid,
+      );
+
+  /// Get uid of current user
+  String getCurrentUID() {
+    return _firebaseAuth.currentUser.uid;
+  }
+
+  /// Get the current user
+  User getCurrentUser() {
+    return _firebaseAuth.currentUser;
+  }
+
+  /// Sign Up With email and password
+  Future<String> createUserWithEmailAndPassword(
+      String email, String password) async {
+    final currentUser = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+
+    return currentUser.user.uid;
+  }
+
+  /// Sign in with email and password
+  Future<String> signInWithEmailAndPassword(
+      String email, String password) async {
+    return (await _firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password))
+        .user
+        .uid;
+  }
+
+  Future<void> signOut() {
+    return _firebaseAuth.signOut();
+  }
+}
